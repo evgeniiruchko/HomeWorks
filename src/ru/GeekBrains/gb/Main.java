2402c86306186047ec9b17e64288aec9558a0719
EvgeniiRuchko <gk1987@mail.ru>
2020-10-27 17:35:13 +0300

    Первое ДЗ

---
+++ src/ru/GeekBrains/gb/Main.java
@@ -0,0 +1,53 @@
+package ru.GeekBrains.gb;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        float a, b, c, d;
+        int i, j;
+        String name;
+        short year;
+
+        System.out.println("Задание №3. Написать метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,где a, b, c, d – входные параметры этого метода");
+        System.out.print("Введите a: ");
+        a = in.nextFloat();
+        System.out.print("Введите b: ");
+        b = in.nextFloat();
+        System.out.print("Введите c: ");
+        c = in.nextFloat();
+        System.out.print("Введите d: ");
+        d = in.nextFloat();
+        System.out.println("Результат = " + HomeWork1.task3(a, b, c, d));
+
+        System.out.println("Задание №4: Написать метод, принимающий на вход два числа, и проверяющий что их сумма лежит в пределах от 10 до 20(включительно), если да – вернуть true, в противном случае – false;");
+        System.out.print("Введите первое число: ");
+        i = in.nextInt();
+        System.out.print("Введите второе число: ");
+        j = in.nextInt();
+        System.out.println("Результат = " + HomeWork1.task4(i, j));
+
+        System.out.println("Задание №5: Написать метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль положительное ли число передали, или отрицательное; Замечание: ноль считаем положительным числом.");
+        System.out.print("Введите целое число: ");
+        i = in.nextInt();
+        System.out.println("Результат = " + HomeWork1.task5(i));
+
+        System.out.println("Задание №6: Написать метод, которому в качестве параметра передается целое число, метод должен вернуть true, если число отрицательное");
+        System.out.print("Введите целое число: ");
+        i = in.nextInt();
+        System.out.println("Результат = " + HomeWork1.task6(i));
+
+        System.out.println("Задание №7: Написать метод, которому в качестве параметра передается строка, обозначающая имя, метод должен вывести в консоль сообщение 'Привет, указанное_имя!'");
+        System.out.print("Введите имя: ");
+        name = in.next();
+        System.out.println(HomeWork1.task7(name));
+
+        System.out.println("* Написать метод, который определяет является ли год високосным, и выводит сообщение в консоль. Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный");
+        System.out.print("Введите год: ");
+        year = in.nextShort();
+        System.out.println(HomeWork1.task8(year));
+    }
+
+}
