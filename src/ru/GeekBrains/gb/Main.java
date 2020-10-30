e848a75c9adfbb787d98057a68bad16d820a62c4
EvgeniiRuchko <gk1987@mail.ru>
2020-10-30 10:41:57 +0300

    сделал в майне выбор проверяемого ДЗ

--- src/ru/GeekBrains/gb/Main.java
+++ src/ru/GeekBrains/gb/Main.java
@@ -1,13 +1,23 @@
 package ru.GeekBrains.gb;
-
+import java.util.Scanner;
 
 
 public class Main {
 
     public static void main(String[] args) {
 
-        HomeWork1.RunHomeWork1();
+        Scanner in = new Scanner(System.in);
+        int homeWorkNumber;
 
+        System.out.print("Домашнее задание к какому номеру урока будем проверять? ");
+        homeWorkNumber = in.nextInt();
+
+        switch (homeWorkNumber) {
+            case 1:
+                HomeWork1.RunHomeWork1();
+                break;
+            default:
+                System.out.println("Такого ДЗ не существует");
+        }
     }
-
 }
