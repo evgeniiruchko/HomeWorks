aa2bc68843daa8f26c70ab3bee5e51fcda67523d
EvgeniiRuchko <gk1987@mail.ru>
2020-11-08 16:44:54 +0300

    Выполненное ДЗ к 4 уроку. Пока без последнего задания

--- src/ru/GeekBrains/gb/GameXO.java
+++ src/ru/GeekBrains/gb/GameXO.java
@@ -5,8 +5,8 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GameXO {
-    public static final int SIZE = 5;
-    public static final int COUNTFORWIN = 4;
+    public static final int SIZE = 3;
+    public static final int COUNTFORWIN = 3;
     public static char[][] map;
 
     public static final char DOT_X = 'X';
@@ -14,15 +14,44 @@ public class GameXO {
     public static final char DOT_EMPTY = '*';
 
     public static void runGameXO(){
+        boolean gameOver = false;
+
         initMap();
+
+//Данные для отладки
+//        map = new char[][]{
+//                {'O', '*', 'O', 'X', 'O'},
+//                {'O', 'X', 'X', 'X', 'O'},
+//                {'X', '*', 'X', 'X', 'X'},
+//                {'O', 'O', 'X', 'O', 'X'},
+//                {'X', 'X', 'X', 'O', '*'},
+//        };
 
         do {
             humanTurn();
-            System.out.println(isVictory(DOT_X));
+            if (isVictory(DOT_X)){
+                System.out.println("Человек выйграл");
+                gameOver = true;
+                break;
+            }
+            if (isDraw()){
+                System.out.println("Ничья");
+                gameOver = true;
+                break;
+            }
 
             compTurn();
-
-        }while (true);
+            if (isVictory(DOT_O)){
+                System.out.println("Машина выйграла");
+                gameOver = true;
+                break;
+            }
+            if (isDraw()){
+                System.out.println("Ничья");
+                gameOver = true;
+                break;
+            }
+        }while (gameOver == false);
 
 
     }
@@ -69,8 +98,8 @@ public class GameXO {
         int x, y;
         System.out.println("введите координаты x и y");
         do {
-            x = in.nextInt() - 1;
             y = in.nextInt() - 1;
+            x = in.nextInt() - 1;
             if (!isValidFild(x,y)){
                 System.out.println("Неверно. Введите от 1 до " + SIZE);
             }
@@ -90,67 +119,105 @@ public class GameXO {
         System.out.println("Ход машины");
         printMap();
     }
+    public static void compTurnTest(){
+        Random random = new Random();
+        int x, y;
+        do {
+            x = random.nextInt(SIZE);
+            y = random.nextInt(SIZE);
+        } while (!isValidFild(x,y));
+        map[x][y] = DOT_O;
+        System.out.println("Ход машины");
+        printMap();
+    }
+
+    public static boolean isDraw(){
+        for (int row = 0; row < SIZE; row++){
+            for (int column = 0; column < SIZE; column++){
+                if (map[row][column] == DOT_EMPTY){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
     public static boolean isVictory(char dot){
-        int horizontScore = 0;
-        int verticalScore = 0;
-        int diagonalScore = 0;
-//сначала сверяю всё по горизонтали
-        for (int row = 0; row <= SIZE - 1; row++){
-            for (int column = 0; column < SIZE - 1; column++){
-                if (map[row][column] == dot && map[row][column+1] == dot){
-                    horizontScore++;
-                    if (horizontScore == COUNTFORWIN-1){
+        int horizontScore;
+        int verticalScore;
+        int diagonalScore;
+        for (int i = 0; i <= SIZE -1; i++){
+            for (int j = 0; j <= SIZE - COUNTFORWIN; j++){
+                horizontScore = 0;
+                verticalScore = 0;
+                diagonalScore = 0;
+//проверка по горизонтали и по вертикали
+                for (int count = 1; count <= COUNTFORWIN; count++){
+                    if (map[i][j + count - 1] == dot){
+                        horizontScore++;
+                    }
+                    else{
+                        horizontScore = 0;
+                    }
+                    if (map[j + count - 1][i] == dot){
+                        verticalScore++;
+                    }
+                    else{
+                        verticalScore = 0;
+                    }
+//Проверка по диагоналям
+                    if (i <= SIZE - COUNTFORWIN) {
+                        if (map[i + count - 1][j + count - 1] == dot) {
+                            diagonalScore++;
+                        } else {
+                            diagonalScore = 0;
+                        }
+                    }
+                    if (i >= COUNTFORWIN - 1) {
+                        if (map[Math.abs(j + count - 1)][Math.abs(i - count + 1)] == dot) {
+                            diagonalScore++;
+                        } else {
+                            diagonalScore = 0;
+                        }
+                    }
+                    if (verticalScore == COUNTFORWIN || horizontScore == COUNTFORWIN || diagonalScore == COUNTFORWIN){
                         return true;
                     }
-                } else {
-                    horizontScore = 0;
                 }
             }
         }
-//тоже самое, но по вертикали
-        for (int column = 0; column <= SIZE-1; column++){
-            for (int row = 0; row < SIZE-1; row++){
-                if (map[row][column] == dot && map[row + 1][column] == dot){
-                    verticalScore++;
-                    if (verticalScore == COUNTFORWIN-1){
-                        return true;
-                    }
-                } else {
-                    verticalScore = 0;
-                }
-            }
-        }
-//по диагонали вправо вниз
-        for (int column = 0; column <= SIZE - COUNTFORWIN; column++) {
-            for (int row = 0; row <= SIZE - COUNTFORWIN; row++) {
-                for (int i = 1; i <= COUNTFORWIN; i++){
-                    if (map[column + i - 1][row + i - 1] == dot){
-                        diagonalScore++;
-                        if (diagonalScore == COUNTFORWIN){
-                            return true;
-                        }
-                    } else{
-                        diagonalScore = 0;
-                    }
-                }
-            }
-        }
-//по диагонали слева вниз
-        for (int column = 0; column <= SIZE - COUNTFORWIN; column++) {
-            for (int row = SIZE - 1; row >= SIZE - COUNTFORWIN; row--) { //тут поправить, а то уходит в минус
-                for (int i = 1; i <= COUNTFORWIN; i++){
-                    if (map[column + i - 1][row - i + 1] == dot){
-                        diagonalScore++;
-                        if (diagonalScore == COUNTFORWIN){
-                            return true;
-                        }
-                    } else{
-                        diagonalScore = 0;
-                    }
-                }
-            }
-        }
+//Проверка по диагоналям старая версия. Выше более оптимизировано
+//        for (int column = 0; column <= SIZE - COUNTFORWIN; column++) {
+////по диагонали направо вниз
+//            for (int row = 0; row <= SIZE - COUNTFORWIN; row++) {
+//                diagonalScore = 0;
+//                for (int i = 1; i <= COUNTFORWIN; i++){
+//                    if (map[column + i - 1][row + i - 1] == dot){
+//                        diagonalScore++;
+//                        if (diagonalScore == COUNTFORWIN){
+//                            return true;
+//                        }
+//                    } else{
+//                        diagonalScore = 0;
+//                    }
+//                }
+//            }
+////по диагонали налево вниз
+//            for (int row = SIZE - 1; row >= COUNTFORWIN - 1; row--) {
+//                diagonalScore = 0;
+//                for (int i = 1; i <= COUNTFORWIN; i++){
+//                    if (map[column + i - 1][row - i + 1] == dot){
+//                        diagonalScore++;
+//                        if (diagonalScore == COUNTFORWIN){
+//                            return true;
+//                        }
+//                    } else{
+//                        diagonalScore = 0;
+//                    }
+//                }
+//            }
+//
+//        }
         return false;
     }
 
