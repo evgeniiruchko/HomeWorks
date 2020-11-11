8ab30e621be6e6608e8b792a911ab17df5f5d4cc
EvgeniiRuchko <gk1987@mail.ru>
2020-11-11 16:59:11 +0300

    Домашнее задание к 5 уроку

--- src/ru/GeekBrains/gb/Main.java
+++ src/ru/GeekBrains/gb/Main.java
@@ -25,6 +25,20 @@ public class Main {
             case 4:
                 GameXO.runGameXO();
                 break;
+            case 5:
+                Worker[] workArray = new Worker[5];
+                workArray[0] = new Worker("Иванов Иван Иванович","Директор", "ivanovII@mail.ru", "9600000001", 100, 55);
+                workArray[1] = new Worker("Петров Пётр Петрович","Зам директора", "PetrovPP@mail.ru", "9600000002", 90, 45);
+                workArray[2] = new Worker("Складов Сергей Анатольевич","Кладовщик", "skladovSA@mail.ru", "9600000003", 70, 30);
+                workArray[3] = new Worker("Колесов Владимир Владимирович","Водитель", "kolesovVV@mail.ru", "9600000004", 70, 40);
+                workArray[4] = new Worker("Приносов Антон Антонович","Грузчик", "gruz@mail.ru", "9600000005", 40, 20);
+                for (int i = 0; i < workArray.length; i++){
+                    if (workArray[i].age > 40){
+                        workArray[i].PrintInfo();
+                    }
+                }
+
+                break;
             default:
                 System.out.println("Такого ДЗ не существует");
         }
