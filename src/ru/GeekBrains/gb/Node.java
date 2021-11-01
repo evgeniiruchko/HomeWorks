480ccbfaed63cd343055164ea3444c2670d91c76
EvgeniiRuchko <gk1987@mail.ru>
2021-11-01 19:24:24 +0300

    Домашняя работа к 4 уроку

---
+++ src/ru/GeekBrains/gb/Node.java
@@ -0,0 +1,42 @@
+package ru.GeekBrains.gb;
+
+class Node<T> {
+    Node prev;
+    T value;
+    Node next;
+
+    public Node(T value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    public Node(Node prev, T value, Node next) {
+        this.prev = prev;
+        this.value = value;
+        this.next = next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+}
