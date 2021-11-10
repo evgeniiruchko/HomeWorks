649d405979a657ef7433e6c34dbbe042edbc59d1
EvgeniiRuchko <gk1987@mail.ru>
2021-11-10 21:19:16 +0300

    Графы

---
+++ src/ru/GeekBrains/gb/graph/BreadthFirstPaths.java
@@ -0,0 +1,27 @@
+package ru.GeekBrains.gb.graph;
+
+import java.util.LinkedList;
+
+public class BreadthFirstPaths extends Paths {
+
+    public BreadthFirstPaths(Graph g, int source) {
+        super(g, source);
+        bfs(g, source);
+    }
+
+    private void bfs(Graph g, int source) {
+        LinkedList<Integer> queue = new LinkedList<>();
+        queue.addFirst(source);
+        marked[source] = true;
+        while (!queue.isEmpty()) {
+            int vertex = queue.removeLast();
+            for (int w : g.getAdjList(vertex)) {
+                if (!marked[w]) {
+                    marked[w] = true;
+                    edgeTo[w] = vertex;
+                    queue.addFirst(w);
+                }
+            }
+        }
+    }
+}
